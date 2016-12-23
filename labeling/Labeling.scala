fe9f539026ab1277bb68b0226629334de40097f3
alkouf <alkouf@hotmail.com>
2016-12-23 04:27:12 +0200

    Stemming and stopwords in class Labeling

--- labeling/Labeling.scala
+++ labeling/Labeling.scala
@@ -1,4 +1,8 @@
+package labeling
+
 import java.io.{File, FileOutputStream, PrintWriter}
+
+import feautext.Stemmer
 
 import scala.io.Source
 
@@ -30,22 +34,27 @@ object Labeling {
     Source.fromFile(datafn).getLines().mkString
   }
 
-  def writetoCSV(file: File, csvfilename: String,flag:Boolean): Unit = {
+  def writetoCSV(file: File, csvfilename: String,flag:Boolean, stemmer: Stemmer, stopWords: List[String]): Unit = {
 
     val pw = new PrintWriter(new FileOutputStream(new File(csvfilename),true))
-      if(flag){
-        pw.println('"'+this.readFiletoString(file)+'"'+",positive")
-      }else{
-        pw.println('"'+this.readFiletoString(file)+'"'+",negative")
-      }
-      pw.close()
+    if(flag){
+      pw.println('"'+stemNstopwords(this.readFiletoString(file),stemmer,stopWords)+'"'+",positive")
+    }else{
+      pw.println('"'+stemNstopwords(this.readFiletoString(file),stemmer,stopWords)+'"'+",negative")
+    }
+    pw.close()
   }
 
-  def produceCSV(samplesdir: String,csvfile: String): Unit ={
+
+  def produceCSV(samplesdir: String,csvfile: String, stemmer: Stemmer, stopWords: List[String]): Unit ={
     this.getListOfSubDir(samplesdir).foreach(d =>if(d.toString.contains("pos")) {
-      this.getListOfFiles(d.toString).foreach(f=>this.writetoCSV(f,csvfile,flag=true))
-      }else{
-      this.getListOfFiles(d.toString).foreach(f=>this.writetoCSV(f,csvfile,flag=true))})
+      println("Number of positive reviews: " +this.getListOfFiles(d.toString).size+ ", at directory: "+d.toString)
+      println("Writing positive reviews to csv...")
+      this.getListOfFiles(d.toString).foreach(f=>this.writetoCSV(f,csvfile,flag=true, stemmer, stopWords))
+    }else {
+      println("Number of negative reviews: " +this.getListOfFiles(d.toString).size+ ", at directory: "+d.toString)
+      println("Writing negative reviews to csv...")
+      this.getListOfFiles(d.toString).foreach(f=>this.writetoCSV(f,csvfile,flag=false, stemmer, stopWords))})
   }
 
   def deletePreviousCSV(csvfilename: String): Unit ={
@@ -53,6 +62,16 @@ object Labeling {
   }
 
   def main(args: Array[String]): Unit = {
+
+    val stemmer =new Stemmer
+
+    //Create a list with the stopwords
+    val stopWords = scala.io.Source.fromFile(System.getProperty("user.dir")+"/stopWords.txt").getLines.toList
+
+    //!!!
+    //toCsv(stemmer, stopWords)
+
+
     var  csvfilename : String =""
     if (args.length != 0) {
       val samplesdir :String = args(0)
@@ -60,7 +79,7 @@ object Labeling {
         if (args.length < 2) {
           csvfilename = "data.csv"
         } else {
-           csvfilename = args(1)
+          csvfilename = args(1)
         }
       }else{
         println("scala Labeling.scala samples_dir_path csv_file_path")
@@ -70,15 +89,79 @@ object Labeling {
       this.deletePreviousCSV(csvfilename)
       println("Process directory :"+samplesdir)
       //---------------------------------------------
-      this.produceCSV(samplesdir,csvfilename)
+      this.produceCSV(samplesdir,csvfilename, stemmer, stopWords)
       //---------------------------------------------
       println("Writing data to :"+csvfilename)
       println("Labeling finished.")
     }else{
       println("Please provide directory path.")
     }
+  }
 
 
+
+  /**
+    * @author Alexandros
+    *
+    * Alternative main <br>
+    * proxeiri methodos pou prospernaei to args[String]
+    *
+    * @param stemmer
+    * @param stopWords
+    */
+  def toCsv(stemmer: Stemmer, stopWords:List[String]): Unit ={
+
+    //the file to write the results
+    val csvfilename = System.getProperty("user.dir")+"/sampleData.csv"
+
+    //the directory where the data are located
+    val datadirectory = System.getProperty("user.dir")+"/sampleData/train"
+
+    println("deleting existing file: "+ csvfilename)
+    this.deletePreviousCSV(csvfilename)
+
+    println("Process directory :"+datadirectory)
+    println("Producing csv...")
+    this.produceCSV(datadirectory,csvfilename, stemmer, stopWords)
+    println("Done")
+
+  }
+
+  /**
+    * @author Alexandros
+    *
+    * Stopwords and Stemming <br>
+    *
+    * Removes the stopwords and finds the root of every word in a sentence <br><br>
+    *
+    * 1. First converts the line to lower case, <br>
+    * 2. Then removes the <em>punctuation<em/>, the <em>numbers<em/>, and the rest <em>special character<em/> (except apostrophe),<br>
+    * 3. After that removes the stopwords, <br>
+    * 4. Applies the stemming process<br>
+    * 5. Finally removes the stopwords that appeared after stemming
+    *
+    * @param line
+    * @param stemmer
+    * @return the clean words of the sentence separated with spaces
+    */
+  def stemNstopwords(line: String, stemmer: Stemmer, stopwords: List[String]): String= {
+    var cleanline =  line.toLowerCase()
+    cleanline = removePunctuation(cleanline)
+    cleanline = cleanline.split(" ").filter(word => !stopwords.contains(word)).mkString(" ")
+    return stemmer.stemLine(cleanline).filter(w => !stopwords.contains(w)).mkString(" ")
+  }
+
+  /**
+    * @author Alexandros
+    *
+    * Removes all special character except for "'" (apostrophe), because the list of stopwords has words like "isn't".
+    *
+    * @param line
+    * @return
+    */
+  def removePunctuation(line: String): String ={
+    val allowedSpecialChars :List[Char] = List('\'', ' ')
+    return line.toCharArray.filter(c => {(allowedSpecialChars.contains(c) || c.isLetter)}).mkString
   }
 
 }
